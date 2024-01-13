6556db45ef19ee0af70d6440bf569fad0ecb0a99
baopro1cute123 <nguyenbao12c6nh1@gmail.com>
2024-01-13 21:09:01 +0700

    ketthuckhoacbclassreact

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,11 +3,32 @@ const initState = {
         {id: 1 , name : 'Bao'},
         {id: 2 , name : 'Bao2'},
 
-    ]
+       
+    ],
+    posts: [],
 }
 const rootReducer = (state = initState, action) =>{
-
-    return state;
+    switch (action.type) {
+        case 'DELETE_USER':
+            console.log(">>> run into delete user", action)
+            //action là dữ liệu bên React gửi qua Redux
+            let users = state.users;
+            users = users.filter(item => item.id !== action.payload.id)
+            return {
+                ...state, users
+            };
+        case 'ADD_USER':
+            let id = Math.floor(Math.random()*1001)
+            let user = {
+                    id : id,
+                    name: `ramdom - ${id}`}
+            return {
+                ...state, users: [...state.users, user]
+            }
+                    
+        default:
+            return state;
+        }
 
 }
 
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -9,10 +9,37 @@ class Home extends React.Component {
 //       }, 3000
 //       )
 //   }
-
+  handleDeleteUser = (user) =>{
+    console.log(">>>>check user", user)
+    this.props.deleteUserRedux(user)
+  }
+  handleCreatUser = () => {
+    this.props.addUserRedux()
+  }
   render() {
     console.log("check data redux", this.props.dataRedux);
-    return <div>Hello My WEBSITE </div>;
+    let listUser = this.props.dataRedux;
+    return (
+      <>
+      <div>Hello My WEBSITE </div>
+      <button onClick={()=>this.handleCreatUser()}>ADD New</button>
+      <input />
+      <div>
+        {listUser && listUser.length > 0 &&
+          listUser.map((item, index)=>{
+            return (
+              <div key={item.id}>
+                {index + 1} - {item.name}-
+                <span onClick={()=>this.handleDeleteUser(item)}>x</span>
+
+              </div>
+            )
+          }
+        )}
+      </div>
+      </>
+    );
+
   }
 }
 
@@ -20,5 +47,22 @@ const mapStateToProps = (state) => {
   return {dataRedux : state.users } //state.user bên rootRedurs
 }
 
-export default connect(mapStateToProps)(HocColor(Home));
+const mapDispatchtoProps = (dispatch) => {
+  return {
+    deleteUserRedux: (userDelete) => {
+      dispatch({
+        type: 'DELETE_USER',
+        payload: userDelete // payload là dữ liệu id muốn xóa
+      });
+      console.log("payload", userDelete);
+    },
+    addUserRedux: () => { 
+        dispatch({
+          type : 'ADD_USER',
+        })
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchtoProps)(HocColor(Home));
 // trong Home kết nối với redux
